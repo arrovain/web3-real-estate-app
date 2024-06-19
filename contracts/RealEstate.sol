cb51ce19e9a4daf740f81f9f8447c228631ed7a8
arrovain <arrovain@gmail.com>
2024-06-19 17:03:58 +0300

    import openzeppelins

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,12 @@
 //SPDX-License-Identifier: Unlicense
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
 pragma solidity ^0.8.0;
 
 contract RealEstate {
+
 
 }
