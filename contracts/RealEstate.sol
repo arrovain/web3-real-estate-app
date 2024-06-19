6c25d18158186d2bfcf908408295dc02b9fc4446
arrovain <arrovain@gmail.com>
2024-06-19 17:08:06 +0300

    MINT function

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -12,5 +12,14 @@ contract RealEstate is ERC721URIStorage  {
 
     constructor() ERC721("Real Estate", "REAL") {}
 
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+
 
 }
