8017adea73dac7e666a0e48b331b6c190eebd33e
arrovain <arrovain@gmail.com>
2024-06-19 17:05:39 +0300

    counter for tokens

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 pragma solidity ^0.8.0;
 
 contract RealEstate is ERC721URIStorage  {
-
+ using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
 }
