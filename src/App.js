65efc4997aa77daac8dad377abe276745ecd499c
arrovain <arrovain@gmail.com>
2024-06-20 15:43:39 +0300

    mapping home

--- src/App.js
+++ src/App.js
@@ -62,9 +62,43 @@ function App() {
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+
       <div className="cards__section">
-        <h3>Welcome to Millow</h3>
+        <h3>Homes For You</h3>
+
+        <hr />
+
+        <div className="cards">
+          {homes.map((home, index) => (
+            <div className="card" key={index} onClick={() => togglePop(home)}>
+              <div className="card__image">
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+
+      {toggle && (
+        <Home
+          home={home}
+          provider={provider}
+          account={account}
+          escrow={escrow}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
